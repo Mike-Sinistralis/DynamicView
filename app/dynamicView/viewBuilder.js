fb7f7a3aac0dfc85f6eb351ef68dca3ea0344a23
mzimmerman <mzimmerman0205@gmail.com>
2015-08-02 21:34:25 -0400

    Finished 1.0

--- app/dynamicView/viewBuilder.js
+++ app/dynamicView/viewBuilder.js
@@ -5,6 +5,7 @@ viewBuilder = (function(){
 
         encounterData = null,
         conbatantTable = null,
+        viewOptions = null,
 
         defCache = {
             encounterDef: null,
@@ -28,7 +29,7 @@ viewBuilder = (function(){
             return dynamicViewList[currentView].encounterDef;
         }
 
-        view = globalView[def];
+        view = globalView[def].slice(0);
 
         viewIterable = dynamicViewList[currentView][def];
 
@@ -77,13 +78,74 @@ viewBuilder = (function(){
 
             headerRow.appendChild(cell);
         }
-
         combatantTable.tHead = tableHeader;
     }
 
     function buildViewBody()
     {
+        var newTableBody = document.createElement("tbody"),
+            oldTableBody = document.getElementById("combatantTableBody"),
+            combatantIndex = 0,
+            combatant,
+            egiSearch,
+            tableRow,
+            cell,
+            currentView = defCache.bodyDef || getCurrentView(defType.BODY);
 
+        newTableBody.id = "combatantTableBody";
+
+        for(var combatantName in currentData.Combatant)
+        {
+            combatant = currentData.Combatant[combatantName];
+            combatant.JobOrName = combatant.Job || "limit break";
+            egiSearch = combatant.JobOrName.indexOf("-Egi (");
+
+            if(egiSearch != -1 ||
+               combatant.JobOrName.indexOf("Eos (") == 0 ||
+               combatant.JobOrName.indexOf("Selene (") == 0 ||
+               combatant.JobOrName.indexOf("Carbuncle (") != -1) 
+            {
+                combatant.JobOrName = "pet";
+            }
+            else if(combatant.JobOrName.indexOf(" (") != -1) 
+            {
+                combatant.JobOrName = "cho";
+            }
+
+            tableRow = newTableBody.insertRow(newTableBody.rows.length);
+
+            for(var i = 0; i < currentView.length; i++)
+            {
+                cell = tableRow.insertCell(i);
+
+                if(currentView[i].html) 
+                {
+                    cell.innerHTML = loadOptions(currentView[i].html, currentView[i].options, combatant);
+                } 
+                else
+                {
+                    cell.innerText = loadOptions(currentView[i].text, currentView[i].options, combatant);
+                }
+
+                cell.style.width = currentView[i].width;
+                cell.style.maxWidth = currentView[i].width;
+
+                if(currentView[i].align) 
+                {
+                    cell.style.textAlign = currentView[i].align;
+                }
+            }
+
+            combatantIndex++;
+        }
+        if(oldTableBody)
+        {
+            combatantTable.replaceChild(newTableBody, oldTableBody);
+        }
+        else
+        {
+            combatantTable.appendChild(newTableBody);
+        }
     }
 
     function buildViewEncounter()
@@ -129,83 +191,26 @@ viewBuilder = (function(){
     };
 
     ViewBuilder.prototype.init = function() {
+        var viewElement,
+            self = this;
+
         encounterData = document.getElementById("encounterData");
         combatantTable = document.getElementById("combatantTable");
+        viewOptions = document.getElementById("views");
+
+        for(var i = 0; i < dynamicViewList.length; i++)
+        {
+            viewElement = document.createElement("span");
+            viewElement.className = "viewOption";
+            viewElement.setAttribute("data-viewIndex", i);
+            viewElement.innerText = dynamicViewList[i].name + "   ";
+            viewElement.addEventListener("mouseenter", function(event) {
+                self.setView(event.target.dataset.viewindex);
+            });
+            viewOptions.appendChild(viewElement);
+        }
     };
 
     return new ViewBuilder();
 }());
 
-function updateCombatantList(data) {
-    var table = document.getElementById('combatantTable');
-    var oldTableBody = table.tBodies.namedItem('combatantTableBody');
-    var newTableBody = document.createElement("tbody");
-    newTableBody.id = "combatantTableBody";
-    var bodyDefine = viewBuilder.buildViewBody();
-
-    var combatantIndex = 0;
-    for (var combatantName in data.Combatant) {
-        var combatant = data.Combatant[combatantName];
-        combatant.JobOrName = combatant.Job || combatantName;
-        var egiSearch = combatant.JobOrName.indexOf("-Egi (");
-        if (egiSearch != -1) {
-            combatant.JobOrName = combatant.JobOrName.substring(0, egiSearch);
-        }
-        else if (combatant.JobOrName.indexOf("Eos (") == 0) {
-            combatant.JobOrName = "Eos";
-        }
-        else if (combatant.JobOrName.indexOf("Selene (") == 0) {
-            combatant.JobOrName = "Selene";
-        }
-        else if (combatant.JobOrName.indexOf("Carbuncle (") != -1) {
-            // currently no carbuncle pics
-        }
-        else if (combatant.JobOrName.indexOf(" (") != -1) {
-            combatant.JobOrName = "choco";
-        }
-
-
-        var tableRow = newTableBody.insertRow(newTableBody.rows.length);
-        for (var i = 0; i < bodyDefine.length; i++) {
-            var cell = tableRow.insertCell(i);
-            // テキスト設定
-            if (typeof bodyDefine[i].text !== 'undefined') {
-                var cellText;
-                if (typeof bodyDefine[i].text === 'function') {
-                    cellText = bodyDefine[i].text(combatant, combatantIndex);
-                } else {
-                    cellText = parseActFormat(bodyDefine[i].text, combatant);
-                }
-                cell.innerText = cellText;
-            } else if (typeof bodyDefine[i].html !== 'undefined') {
-                var cellHTML;
-                if (typeof bodyDefine[i].html === 'function') {
-                    cellHTML = bodyDefine[i].html(combatant, combatantIndex);
-                } else {
-                    cellHTML = parseActFormat(bodyDefine[i].html, combatant);
-                }
-                cell.innerHTML = cellHTML;
-            }
-            // 幅設定
-            cell.style.width = bodyDefine[i].width;
-            cell.style.maxWidth = bodyDefine[i].width;
-            // 行構え設定
-            if (typeof (bodyDefine[i].align) !== 'undefined') {
-                cell.style.textAlign = bodyDefine[i].align;
-            }
-            // エフェクト実行
-            if (typeof bodyDefine[i].effect === 'function') {
-                bodyDefine[i].effect(cell, combatant, combatantIndex);
-            }
-        }
-        combatantIndex++;
-    }
-
-    // tbody が既に存在していたら置換、そうでないならテーブルに追加
-    if (oldTableBody != void (0)) {
-        table.replaceChild(newTableBody, oldTableBody);
-    }
-    else {
-        table.appendChild(newTableBody);
-    }
-}
--- app/dynamicView/viewBuilderConfig.js
+++ app/dynamicView/viewBuilderConfig.js
@@ -4,8 +4,8 @@
     So say you want to add attacks missed to the damageView. You would simply add:
 
     { text: "Misses", width: 3%, align:"center" } to the headerDef, and
-    { text: combatantOptions.MISSES, width: 3%, align: "center" } to the bodyDef.
-    You may substitue text for html as needed. HTML will always take precedence over text. If you are not using HTML, make sure you say the html field to null.
+    { text: "{0}", options: [combatantOptions.MISSES], width: 3%, align: "center" } to the bodyDef.
+    You may substitue text for html as needed. HTML will always take precedence over text.
     Again, refer to config.js to see what options are available to you.
 
     If you want to edit encounterDef, make sure you use encounterOptions instead.
@@ -29,8 +29,8 @@ var enableDynamicSort = false,
             { text: "Name", width: "3%", align: "left" }
         ],
         bodyDef: [
-            { html: "<img src='./images/default/{JobOrName}.png' style='width=1.5%;height:auto;' />", align: "center" },
-            { text: combatantOptions.NAME, width: "3%",  align: "left" }
+            { html: "<img src='./images/default/{0}.png' style='width=1.5%;height:auto;' />", options: [combatantOptions.JOB_OR_NAME], align: "center" },
+            { text: "{0}", options: [combatantOptions.NAME], width: "3%",  align: "left" }
         ]
     },
 
@@ -40,24 +40,24 @@ var enableDynamicSort = false,
         */
         damageView = {
             headerDef: [
-                { text: "DPS" , width: "3%", align: "center", span: 2 },
+                { text: "DPS" , width: "3%", align: "left", span: 2 },
                 { text: "Crit %", width: "1.5%", align: "left" },
                 { text: "Acc %", width: "2%", align: "left" },
                 { text: "Max Hit", width: "4%", align: "left"}
             ],
             bodyDef: [
-                { text: combatantOptions.DPS, width: "1.5%", align: "left" },
-                { text: combatantOptions.DAMAGE_PERCENT, width: "1.5%", align: "left" },
-                { text: combatantOptions.CRITICAL_PERCENTAGE, width: "1.5%", align: "left" },
-                { text: combatantOptions.TO_HIT, width: "2%", align: "left" },
-                { text: combatantOptions.MAX_HIT, width: "4%", align: "left"}
+                { text: "{0}", options: [combatantOptions.DPS], width: "1.5%", align: "left" },
+                { text: "{0}", options: [combatantOptions.DAMAGE_PERCENT], width: "1.5%", align: "left" },
+                { text: "{0}", options: [combatantOptions.CRITICAL_PERCENTAGE], width: "1.5%", align: "left" },
+                { text: "{0}", options: [combatantOptions.TO_HIT], width: "2%", align: "left" },
+                { text: "{0}", options: [combatantOptions.MAX_HIT], width: "4%", align: "left"}
             ],
             encounterDef: {
                 text: "Time: {0}  /  DPS: {1}  /  Damage Dealt: {2}",
-                html: null,
                 options: [encounterOptions.DURATION, encounterOptions.DPS_ROUNDED, encounterOptions.DAMAGE]
             },
-            sortDef: combatantOptions.DPS
+            sortDef: combatantOptions.DPS,
+            name: "Damage"
         },
 
         /*
@@ -66,23 +66,23 @@ var enableDynamicSort = false,
         healedView = {
             headerDef: [
                 { text: "HPS", width: "3%", align: "left", span: 2 },
-                { text: "Heal Crit %", width: "2%", align: "left" },
-                { text: "OverHeal %", width: "2%", align: "left" },
+                { text: "Crit %", width: "2%", align: "left" },
+                { text: "OverH %", width: "2%", align: "left" },
                 { text: "Max Heal", width: "4%", align: "left"}
             ],
             bodyDef: [
-                { text: combatantOptions.HPS, width: "1.5%", align: "left" },
-                { text: combatantOptions.HEALED_PERCENTAGE, width: "2%", align: "left"},
-                { text: combatantOptions.CRITICAL_HEALS_PERCENTAGE, width: "2%", align: "left" },
-                { text: combatantOptions.OVERHEAL_PERCENT, width: "2%", align: "left" },
-                { text: combatantOptions.MAXHEAL, width: "4%", align: "left"}
+                { text: "{0}", options: [combatantOptions.HPS], width: "1.5%", align: "left" },
+                { text: "{0}", options: [combatantOptions.HEALED_PERCENT], width: "1.5%", align: "left"},
+                { text: "{0}", options: [combatantOptions.CRITICAL_HEALS_PERCENTAGE], width: "2%", align: "left" },
+                { text: "{0}", options: [combatantOptions.OVERHEAL_PERCENT], width: "2%", align: "left" },
+                { text: "{0}", options: [combatantOptions.MAX_HEAL], width: "4%", align: "left"}
             ],
             encounterDef: {
                 text: "Time: {0}  /  HPS: {1}  /  Healing Done: {2}",
-                html: null,
                 options: [encounterOptions.DURATION, encounterOptions.HPS_ROUNDED, encounterOptions.HEALED]
             },
-            sortDef: combatantOptions.HPS
+            sortDef: combatantOptions.HPS,
+            name: "Healing"
         },
 
         /*
@@ -90,22 +90,22 @@ var enableDynamicSort = false,
         */
         tankView = {
             headerDef: [
-                { text: "Block %", width: "3%", align: "left" },
+                { text: "Block %", width: "2%", align: "left" },
                 { text: "Parry %", width: "2%", align: "left" },
-                { text: "Damage Taken", width: "2%", align: "left" },
+                { text: "Damage Taken", width: "4%", align: "left" },
                 { text: "Healing Taken", width: "4%", align: "left"}
             ],
             bodyDef: [
-                { text: combatantOptions.PARRY_RATE, width: "1.5%", align: "left" },
-                { text: combatantOptions.BLOCK_RATE, width: "2%", align: "left"},
-                { text: combatantOptions.DAMAGE_TAKEN, width: "2%", align: "left" },
-                { text: combatantOptions.HEALS_TAKEN, width: "2%", align: "left" },
+                { text: "{0}", options: [combatantOptions.PARRY_RATE], width: "2%", align: "left" },
+                { text: "{0}", options: [combatantOptions.BLOCK_RATE], width: "2%", align: "left"},
+                { text: "{0}", options: [combatantOptions.DAMAGE_TAKEN], width: "4%", align: "left" },
+                { text: "{0}", options: [combatantOptions.HEALS_TAKEN], width: "4%", align: "left" },
             ],
             encounterDef: {
                 text: "Time: {0}  /  DPS: {1}  /  HPS: {2}",
-                html: null,
                 options: [encounterOptions.DURATION, encounterOptions.DPS_ROUNDED, encounterOptions.HPS_ROUNDED]
             },
-            sortDef: combatantOptions.DAMAGE_TAKEN
+            sortDef: combatantOptions.DAMAGE_TAKEN,
+            name: "Tanking"
         }
     ];
